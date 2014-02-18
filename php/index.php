b6a1b9d70391d439af7ca45b92c419256a6cccfb
Jacob Hegna <jacobhegna@gmail.com>
2014-02-18 16:17:53 -0600

    Initial commit

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php
+    $page = 
+'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>BVSW Debate</title>
+</head>
+<body>
+    <a>Hello, world!</a>
+</body>
+</html>';
+
+    echo $page;
+?>
