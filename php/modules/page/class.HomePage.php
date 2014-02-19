b0e45b54245b2c20e2ab857d4658814e9c31b9b3
Jacob Hegna <jacobhegna@gmail.com>
2014-02-18 18:33:53 -0600

    I'm honestly sorry about the commits

--- php/modules/page/class.HomePage.php
+++ php/modules/page/class.HomePage.php
@@ -6,7 +6,10 @@ class HomePage extends Page {
 
     public function writePageContent() {
         $content = 
-'';
+'<div class="jumbotron">
+    <h1>BVSW Debate</h1>
+    <h3>#NoDaysOff</h3>
+</div>';
 
         echo $content;
     }
