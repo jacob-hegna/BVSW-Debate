14de1fb01acb248f2098836a55b9a7f46b420191
Jacob Hegna <jacobhegna@gmail.com>
2015-04-27 16:11:21 -0500

    changed some info

--- pages/about.php
+++ pages/about.php
@@ -6,7 +6,6 @@ function get_about() {
     <div class="container" style="text-align:left;">
         <br>
         <p>This is the (un)official website for the Blue Valley Southwest debate team.  We compete on a local, state-wide, and national level and have found success at all three.</p>
-        <p>The website is being developed by <a target="_blank" href="http://jacob-hegna.github.io/">Jacob Hegna</a> and is running on a debian server at his house.  It is fully open source and any contributions are welcome at the official <a target="_blank" href="https://github.com/jacob-hegna/bvswdebate">github repository</a>.</p>
     </div>
 </div>';
     echo $content;
